62c18419bebfd46fe668cefde2cf7b11382c327c
Kurt <kurtcagulang@gmail.com>
2025-08-02 20:16:09 +0800

    change env apiUrl to use prod

---
+++ src/app/report_card/report-card-component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+
+@Component({standalone: true, templateUrl: 'report-card.component.html'})
+export class ReportCardComponent{
+  
+} 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://senior-high-grading-system-api.onrender.com'
 };
