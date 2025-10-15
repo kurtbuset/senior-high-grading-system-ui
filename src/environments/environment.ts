5b0d761a45bff86fec2ce7366545efa9ca45ad4a
Kort Buset <kurtcagulang@gmail.com>
2025-10-15 19:22:55 +0800

    change env backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:4000'
-  apiUrl: 'grading-system.up.railway.app'
+  apiUrl: 'https://grading-system.up.railway.app'
 };
 
 /*
